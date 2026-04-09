fd2723ff7d0f4e3166c0927190f144a2ee81572c
CRA2001 <carlosarro182@gmail.com>
2026-04-09 13:47:05 +0300

    Added a new window for data entry

--- app.py
+++ app.py
@@ -3,13 +3,21 @@ import sys
 dpg.create_context()
 dpg.create_viewport(title = "Heat Stress Index Research App", width=600,height=300)
 
+
+def open_data_entry(sender,user_data):
+    if dpg.does_item_exist("data_entry_window"):
+        dpg.delete_item("data entry window")
+    with dpg.window(tag="data_entry_window",label="Data Entry",width = 400,height=350,pos=[100,50]):
+        dpg.add_text("Enter heat stress data.")
+        dpg.add_separator()
 def exit(sender,user_data):
     sys.exit("Exiting")
 
 with dpg.window(tag="Window1"):
     dpg.add_text("Heat Stress Index Research App")
-    button1 = dpg.add_button(label="Data Entry",callback=dataEntry,user_data="Data Entry")
+    button1 = dpg.add_button(label="Data Entry",callback=open_data_entry,user_data="data_entry")
     button2 = dpg.add_button(label="Data View")
+    #exit button
     button3 = dpg.add_button(label="Exit",callback=exit,user_data="user exit")
 
 dpg.setup_dearpygui()
